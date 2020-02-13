7bdf6f00e0a2da149da013747100e4968ad472d8
GET Rikard <rikard@getacademy.no>
2020-02-13 16:58:48 +0100

    Console.OutputEncoding = Encoding.UTF8;

--- RRSSR/Program.cs
+++ RRSSR/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel.Syndication;
+using System.Text;
 using System.Xml;
 
 namespace RRSSR
@@ -33,6 +34,7 @@ namespace RRSSR
             Console.Title = "RRSSR";
             Console.BackgroundColor = Settings.BgColor;
             Console.ForegroundColor = Settings.FgColor;
+            Console.OutputEncoding = Encoding.UTF8;
             Console.Clear();
 
             if (Settings.Urls.Count == 0)
